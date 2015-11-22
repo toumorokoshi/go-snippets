6c34112edbad314c06029e21130475d8ee269646
Syohei YOSHIDA <syohex@gmail.com>
2015-11-22 09:33:31 +0900

    Use defvar for declaring global variable

--- go-snippets.el
+++ go-snippets.el
@@ -26,8 +26,8 @@
 
 ;;; Code:
 
-(setq go-snippets-dir (file-name-directory (or (buffer-file-name)
-                                                load-file-name)))
+(defvar go-snippets-dir (file-name-directory (or (buffer-file-name)
+                                                 load-file-name)))
 
 ;;;###autoload
 (defun go-snippets-initialize ()
